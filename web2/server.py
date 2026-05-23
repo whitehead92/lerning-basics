69efe7e0ff5125c565b8d103f7bb8ec8e0f32c65
SanFranXX <krutenthiksasha@mail.ru>
2026-05-23 10:35:01 +0300

    добавил 2 формы

--- web2/server.py
+++ web2/server.py
@@ -1,6 +1,7 @@
 import http.server
 import socketserver
 from urllib.parse import urlparse, parse_qs
+import json
 
 PORT = 8000
 
@@ -13,21 +14,46 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             login = p.get("login", [""])[0]
             password = p.get("password", [""])[0]
             
-            self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=utf-8")
-            self.end_headers()
-            
-            html = f"""<!DOCTYPE html><html><body>
-            <h3>Получено:</h3>
-            <p>Login: {login}</p>
-            <p>Password: {password}</p>
-            <a href="/index.html">Вернуться на главную</a>
-            </body></html>"""
-            self.wfile.write(html.encode("utf-8"))
+            self._result(login, password)
             return
-        
+            
+        if self.path == "/":
+            self.path = "/index.html"
         super().do_GET()
+        
+    def do_POST(self):
+        URL = urlparse(self.path)
+        length = int(self.headers.get('Content-Length', 0))
+        body = self.rfile.read(length).decode('utf-8')
+        
+        if URL.path == "/auth":
+            data = parse_qs(body)
+            login = data.get("login", [""])[0]
+            password = data.get("password", [""])[0]
+            self._result(login, password)
+            
+        elif URL.path == "/auth-json":
+            data = json.loads(body)
+            login = data.get("login", "")
+            password = data.get("password", "")
+            self._result(login, password)
+        else:
+            self.send_response(404)
+            self.end_headers()
 
+    def _result(self, login, password):
+        html = f"""<!DOCTYPE html><html><body>
+        <h3>Получено:</h3>
+        <p>Login: {login}</p>
+        <p>Password: {password}</p>
+        <a href="/index.html">Вернуться на главную</a>
+        </body></html>"""
+        
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(html.encode("utf-8"))
+        
 if __name__ == "__main__":
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
         print(f"server start: http://localhost:{PORT}")
